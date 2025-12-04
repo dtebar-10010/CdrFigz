d964ebd1bb629e5cb6809070702b0e91ef300cf8
dtebar <dtebar@gmail.com>
2025-12-04 02:44:55 -0500

    auto-commit

--- grand_v2_settings/wsgi.py
+++ grand_v2_settings/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mtb_v4_project project.
+WSGI config for grand_v3 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -7,10 +7,27 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
+import sys
 import os
 
-from django.core.wsgi import get_wsgi_application
+# Add your project directory to the sys.path (for PythonAnywhere)
+production_path = "/home/dtebar/grand"
+if os.path.exists(production_path):
+    sys.path.append(production_path)
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grand_v2_settings.settings')
+# Set environment variables for production
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "grand_v2_settings.settings")
+os.environ.setdefault("DJANGO_DEBUG", "False")
+os.environ.setdefault(
+    "DJANGO_ALLOWED_HOSTS",
+    "commander-figz.com,www.commander-figz.com,dtebar.pythonanywhere.com",
+)
+os.environ.setdefault(
+    "DJANGO_CSRF_TRUSTED_ORIGINS",
+    "https://commander-figz.com,https://www.commander-figz.com",
+)
+
+# Import the Django WSGI application
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
